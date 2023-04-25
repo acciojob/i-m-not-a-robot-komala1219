c2ae3c4bc663f2a5c18fc482581c33529b05de44
komala1219 <123307320+komala1219@users.noreply.github.com>
2023-04-25 11:49:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,78 +1,119 @@
 //your JS code here. If required.
 
-// Select all the required DOM elements
-const images = document.querySelectorAll(".tile");
-const resetBtn = document.getElementById("reset");
-const verifyBtn = document.getElementById("verify");
-const para = document.getElementById("para");
-const h3 = document.getElementById("h");
+ 
 
-let firstSelectedTile = null;
-let secondSelectedTile = null;
-let isVerified = false;
 
-// Shuffle the array of images and render them on the DOM
-const shuffledImages = shuffleArray([...images]);
-shuffledImages.forEach((image, index) => {
-  image.classList.add(`img${index + 1}`);
+const images = Array.from(document.querySelectorAll("img"));
+// const resetButton = document.getElementById("reset");
+// const verifyButton = document.getElementById("verify");
+const messagePara = document.getElementById("para");
+const heading = document.getElementById("h");
+
+
+
+
+// Get a reference to the images container
+const imagesContainer = document.querySelector(".images");
+
+// Create a new image element and set its attributes
+const newImage = document.createElement("img");
+newImage.classList.add("tile", "img6");
+
+// Randomly select an image from the existing array
+const randomIndex = Math.floor(Math.random() * images.length);
+const randomImage = images[randomIndex];
+
+// Set the new image's source to the randomly selected image's source
+newImage.src = randomImage.src;
+
+// Add the new image to the container and the array
+imagesContainer.appendChild(newImage);
+images.push(newImage);
+
+
+
+
+// Get a reference to the reset button and verify button
+const resetButton = document.querySelector("#reset");
+const verifyButton = document.querySelector("#verify");
+
+// Get a reference to the message paragraph
+const message = document.querySelector("#para");
+
+// Initialize variables to keep track of clicked tiles
+let firstClickedTile = null;
+let secondClickedTile = null;
+
+// Add event listeners to each image
+images.forEach(image => {
+image.addEventListener("click", event => {
+// If both tiles have been clicked, return early
+if (firstClickedTile !== null && secondClickedTile !== null) {
+return;
+}
+
+ 
+// Get a reference to the clicked tile
+const clickedTile = event.target;
+
+// If the clicked tile is already selected, return early
+if (clickedTile.classList.contains("selected")) {
+  return;
+}
+
+// If no tiles have been clicked yet, set this tile as the first clicked tile
+if (firstClickedTile === null) {
+  firstClickedTile = clickedTile;
+  firstClickedTile.classList.add("selected");
+  
+  // Show the reset button
+  resetButton.style.display = "inline-block";
+} 
+// If one tile has been clicked, set this tile as the second clicked tile
+else {
+  secondClickedTile = clickedTile;
+  secondClickedTile.classList.add("selected");
+  
+  // Show the verify button
+  verifyButton.style.display = "inline-block";
+}
+});
 });
 
-// Add click event listener to each image
-images.forEach((image) => {
-  image.addEventListener("click", handleClick);
+// Add event listener to reset button
+resetButton.addEventListener("click", () => {
+// Clear clicked tiles and remove selected class from all tiles
+firstClickedTile = null;
+secondClickedTile = null;
+images.forEach(image => image.classList.remove("selected"));
+
+// Hide the reset button and verify button
+resetButton.style.display = "none";
+verifyButton.style.display = "none";
+
+// Clear the message
+message.textContent = "";
 });
 
-// Add click event listener to reset button
-resetBtn.addEventListener("click", handleReset);
-
-// Add click event listener to verify button
-verifyBtn.addEventListener("click", handleVerify);
-
-// Shuffle the array of images
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
+// Add event listener to verify button
+verifyButton.addEventListener("click", () => {
+// If both tiles are identical, show success message
+if (firstClickedTile.src === secondClickedTile.src) {
+message.textContent = "You are a human. Congratulations!";
+}
+// If tiles are not identical, show error message
+else {
+message.textContent = "We can't verify you as a human. You selected the non-identical tiles.";
 }
 
-// Handle the click event on an image tile
-function handleClick() {
-  if (isVerified) return; // do nothing if already verified
-  if (!firstSelectedTile) {
-    firstSelectedTile = this;
-    firstSelectedTile.classList.add("selected");
-    resetBtn.style.display = "block";
-    return;
-  }
-  if (firstSelectedTile === this) return; // do nothing if same tile clicked twice
-  secondSelectedTile = this;
-  secondSelectedTile.classList.add("selected");
-  verifyBtn.style.display = "block";
-}
+// Remove selected class from all tiles
+images.forEach(image => image.classList.remove("selected"));
 
-// Handle the click event on the reset button
-function handleReset() {
-  firstSelectedTile.classList.remove("selected");
-  secondSelectedTile.classList.remove("selected");
-  firstSelectedTile = null;
-  secondSelectedTile = null;
-  resetBtn.style.display = "none";
-  verifyBtn.style.display = "none";
-  para.style.display = "none";
-}
+// Hide the reset button and verify button
+resetButton.style.display = "none";
+verifyButton.style.display = "none";
 
-// Handle the click event on the verify button
-function handleVerify() {
-  if (!firstSelectedTile || !secondSelectedTile) return; // do nothing if less than 2 tiles selected
-  if (firstSelectedTile.classList[1] === secondSelectedTile.classList[1]) {
-    para.innerHTML = "You are a human. Congratulations!";
-  } else {
-    para.innerHTML = "We can't verify you as a human. You selected the non-identical tiles.";
-  }
-  para.style.display = "block";
-  verifyBtn.style.display = "none";
-  isVerified = true;
-}
-
+// Clear clicked tiles
+firstClickedTile = null;
+secondClickedTile = null;
+});
